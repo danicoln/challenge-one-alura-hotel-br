605bdf1da8079db8ab9be774a210eb87a4b3eb47
Daniel Lincoln <lincolndeveloper@outlook.com>
2023-05-22 15:01:47 -0300

    Criada a entidade Usuario

--- src/domain/usuario/Usuario.java
+++ src/domain/usuario/Usuario.java
@@ -1,7 +1,10 @@
 package domain.usuario;
 
+import java.util.Objects;
+
 public class Usuario {
 
+    private Integer id;
     private String login;
     private String senha;
 
@@ -16,5 +19,18 @@ public class Usuario {
 
     public String getSenha() {
         return senha;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return id.equals(usuario.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
